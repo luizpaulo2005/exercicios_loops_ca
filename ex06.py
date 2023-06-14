0d2d4c483fa9531a3e70942f647ead8e3d0f6b35
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-06-14 12:15:46 -0300

    ex06

--- ex06.py
+++ ex06.py
@@ -1,8 +1,16 @@
+# Crie uma lista com 100 números aleatórios. Percorra esta lista e imprima a posição e o valor do primeiro elemento contido na lista que seja par, e a posição e o valor do elemento imediatamente após este elemento.
+
 from random import randint
 
-numeros = [randint(0, 100) for num in range(100)]
+lista = [randint(1, 100) for i in range(100)]
 
-div2 = [num for num in numeros if num % 2 == 0]
+valor_atual = 0
+valor_anterior = 0
 
-while True:
-    nu
+for i in range(len(lista)):
+    if lista[i] % 2 == 0:
+        valor_atual = i
+        valor_anterior = i - 1
+        print(f'Elemento Atual: {valor_atual} - Valor: {lista[valor_atual]}')
+        print(f'Elemento Anterior: {valor_anterior} - Valor: {lista[valor_anterior]}')
+        break
