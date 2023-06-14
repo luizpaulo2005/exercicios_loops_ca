395c1ccfe4cd622a5dbcdc26565f2ec7c222038f
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-06-14 12:38:15 -0300

    correcao ex08

--- ex08.py
+++ ex08.py
@@ -1,9 +1,12 @@
 numeros = [num for num in range(10)]
-
+numeros_reverso = []
 i = 0
 n = len(numeros) - 1
 
+
 while i < len(numeros):
-    print(numeros[n])
+    numeros_reverso.append(numeros[n])
     i += 1
     n -= 1
+
+print(numeros_reverso)
